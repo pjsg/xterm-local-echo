30dd03316fec092876fc113c1d7583da0d1eee4d
kobakazu0429 <kobakazu10@gmail.com>
2021-08-29 01:57:35 +0900

    fix cursor and add test

--- src/HistoryController.ts
+++ src/HistoryController.ts
@@ -15,7 +15,10 @@ export class HistoryController {
     if (entry.trim() === "") return;
     // Skip duplicate entries
     const lastEntry = this.entries[this.entries.length - 1];
-    if (entry == lastEntry) return;
+    if (entry === lastEntry) {
+      this.cursor = this.entries.length;
+      return;
+    }
     // Keep track of entries
     this.entries.push(entry);
     if (this.entries.length > this.size) {
---
+++ tests/HistoryController.test.ts
@@ -0,0 +1,41 @@
+import { HistoryController } from "./../src/HistoryController";
+
+describe("HistoryController", () => {
+  test("history size", () => {
+    const historySize = 3;
+    const historyController = new HistoryController(historySize);
+
+    historyController.push("1");
+    historyController.push("2");
+    historyController.push("3");
+    historyController.push("4");
+    historyController.push("5");
+
+    expect(historyController["entries"]).toEqual(["3", "4", "5"]);
+  });
+
+  test("history.getPrevious(), .getNext()", () => {
+    const historySize = 10;
+    const historyController = new HistoryController(historySize);
+
+    expect(historyController.getPrevious()).toBeUndefined();
+
+    historyController.push("1");
+    historyController.push("2");
+    historyController.push("3");
+    historyController.push("3");
+
+    expect(historyController.getNext()).toBeUndefined();
+    expect(historyController.getPrevious()).toEqual("3");
+
+    historyController.push("3");
+    historyController.push("3");
+
+    expect(historyController.getPrevious()).toEqual("3");
+    expect(historyController.getPrevious()).toEqual("2");
+    expect(historyController.getPrevious()).toEqual("1");
+    expect(historyController.getNext()).toEqual("2");
+    expect(historyController.getNext()).toEqual("3");
+    expect(historyController.getNext()).toBeUndefined();
+  });
+});
